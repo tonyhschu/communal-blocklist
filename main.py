563da279adebbeb2b69c68b55ea6ed6b8d2a7304
Tony Chu <tony@siftscience.com>
2014-10-19 21:26:01 -0700

    Trying to Flask-dance to work. Failing.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import os
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","postgresql://localhost/communal_blocklist")
+app.config.from_pyfile('config.py')
 
-db = SQLAlchemy(app)
+# os.environ variable are either in .env (locally), or set via heroku config:set TWITTER_KEY=XXXXXXX
+twitter_blueprint = make_twitter_blueprint(
+    api_key=os.environ['TWITTER_KEY'],
+    api_secret=os.environ['TWITTER_SECRET'],
+    redirect_url="http://localhost:5000/",
+)
+app.register_blueprint(twitter_blueprint, url_prefix="/login")
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    if not twitter.authorized:
+        return redirect(url_for("twitter.login"))
 
+    resp = twitter.get("/user")
+    assert resp.ok
+    return "You are @{login} on Twitter".format(login=resp.json()["login"])
+
+if __name__ == "__main__":
+    app.run()
