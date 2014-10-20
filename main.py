2a5c906de339a9cbd69c1e6ae47c927cbe38c07a
Tony Chu <tony@siftscience.com>
2014-10-19 18:50:49 -0700

    boilerplate code flaskDB code?
    via:
    http://blog.y3xz.com/blog/2012/08/16/flask-and-postgresql-on-heroku …
    and other stuff.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","postgresql://localhost/communal_blocklist")
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
