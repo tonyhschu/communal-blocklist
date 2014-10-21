b265459145ea6307d1f405af5ffa3a81ec264357
Tony Chu <tony@siftscience.com>
2014-10-20 22:05:47 -0700

    Ok. This might actually work.

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 # os.environ variable are either in .env (locally), or set via heroku config:set TWITTER_KEY=XXXXXXX
 twitter_blueprint = make_twitter_blueprint(
     api_key=os.environ['TWITTER_KEY'],
     api_secret=os.environ['TWITTER_SECRET'],
-    redirect_url="http://localhost:5000/",
+    redirect_url="http://127.0.0.1:5000",
 )
 app.register_blueprint(twitter_blueprint, url_prefix="/login")
 
@@ -18,9 +19,10 @@ def index():
     if not twitter.authorized:
         return redirect(url_for("twitter.login"))
 
-    resp = twitter.get("/user")
+    resp = twitter.get("account/verify_credentials.json")
     assert resp.ok
-    return "You are @{login} on Twitter".format(login=resp.json()["login"])
+
+    return "You are @{screen_name} on Twitter".format(screen_name=resp.json()["screen_name"])
 
 if __name__ == "__main__":
     app.run()
