0fa4ccfbb767233959f9c4f96e6c40527ac33d22
Tony Chu <tony@siftscience.com>
2014-11-01 15:33:24 -0700

    Ugh
    ???
    https://realpython.com/blog/python/flask-by-example-part-2-postgres-sqla
    lchemy-and-alembic/

--- communalblocklist/__init__.py
+++ communalblocklist/__init__.py
@@ -1,10 +1,13 @@
 import os
 from flask import Flask
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
+from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
 app.config['DEBUG'] = os.environ['DEBUG']
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE_URL']
 
 # os.environ variable are either in .env (locally), or set via heroku config:set TWITTER_KEY=XXXXXXX
 twitter_blueprint = make_twitter_blueprint(
@@ -16,3 +19,5 @@ app.register_blueprint(twitter_blueprint, url_prefix="/login")
 
 import communalblocklist.views
 import communalblocklist.api
+import communalblocklist.models
+
---
+++ communalblocklist/models.py
@@ -0,0 +1,33 @@
+from communalblocklist import app
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+from flask_dance.models import OAuthConsumerMixin
+
+db = SQLAlchemy(app)
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    handle = db.Column(db.String(15), nullable=False, primary_key=True)
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+class OAuth(db.Model, OAuthConsumerMixin):
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
