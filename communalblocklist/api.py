7943c51cc1f5b299096aef8eb5bdea163c6072de
Tony Chu <tony@siftscience.com>
2014-11-02 09:57:00 -0800

    First pass at writing the API side.

--- communalblocklist/api.py
+++ communalblocklist/api.py
@@ -1,11 +1,12 @@
 from communalblocklist import app
+from flask import request
 from flask_dance.contrib.twitter import twitter
-from flask.ext import restful
+from flask.ext.restful import reqparse, abort, Api, Resource
 from flask.ext.login import login_user, logout_user, current_user, login_required
 
-api = restful.Api(app)
+api = Api(app)
 
-class CurrentUser(restful.Resource):
+class CurrentUser(Resource):
     @login_required
     def get(self):
         resp = twitter.get("account/verify_credentials.json")
@@ -17,13 +18,25 @@ class CurrentUser(restful.Resource):
           "twitter": resp.json()
         }
 
-api.add_resource(CurrentUser, '/api/current_user')
+parser = reqparse.RequestParser()
 
-class Blocks(restful.Resource):
+# Add Blocks
+parser.add_argument('t_id', type=int, help='Rate cannot be converted')
+parser.add_argument('topic', type=str)
+
+class Blocks(Resource):
     @login_required
     def get(self):
         resp = twitter.get("blocks/ids.json")
 
         return resp.json()
 
+    @login_required
+    def post(self):
+        args = parser.parse_args()
+
+        return args, 201
+
+
+api.add_resource(CurrentUser, '/api/current_user')
 api.add_resource(Blocks, '/api/blocks')
